0eb314cc39e89e451075d0ccafd201124824ee59
spekary <spekary@gmail.com>
2017-05-11 09:58:14 -0700

    i18n case fixes

--- codegen/templates/db_orm/model_connector/delete_object.tpl.php
+++ codegen/templates/db_orm/model_connector/delete_object.tpl.php
@@ -5,7 +5,7 @@
     public function delete<?= $objTable->ClassName; ?>()
     {
 <?php foreach ($objTable->ManyToManyReferenceArray as $objManyToManyReference) { ?>
-        $this-><?= $objCodeGen->ModelVariableName($objTable->Name) ?>->unassociateAll<?= $objManyToManyReference->ObjectDescriptionPlural ?>();
+        $this-><?= $objCodeGen->modelVariableName($objTable->Name) ?>->unassociateAll<?= $objManyToManyReference->ObjectDescriptionPlural ?>();
 <?php } ?>
-        $this-><?= $objCodeGen->ModelVariableName($objTable->Name); ?>->delete();
+        $this-><?= $objCodeGen->modelVariableName($objTable->Name); ?>->delete();
     }
--- i18n/i18n-lib.inc.php
+++ i18n/i18n-lib.inc.php
@@ -11,32 +11,32 @@ const I18N_DOMAIN = 'qcubed/application'; // replace this with your package name
 /**
  * Translation function specific to your package.
  *
- * @param string $strMsgId			String to translate
+ * @param string $strmsgid			String to translate
  * @param string|null $strContext	Context string, if the same string gets translated in different ways depending on context
  * @return string
  */
-function t($strMsgId, $strContext = null)
+function t($strmsgid, $strContext = null)
 {
 	if (class_exists("\\QCubed\\I18n\\TranslationService") && TranslationService::instance()->translator()) {
 		if (!defined (I18N_DOMAIN . '__BOUND')) {
 			define(I18N_DOMAIN . '__BOUND', 1);
 			TranslationService::instance()->bindDomain(I18N_DOMAIN, __DIR__);	// bind the directory containing the .po files to my domain
 		}
-		return TranslationService::instance()->translate($strMsgId, I18N_DOMAIN, $strContext);
+		return TranslationService::instance()->translate($strmsgid, I18N_DOMAIN, $strContext);
 	}
-	return $strMsgId;
+	return $strmsgid;
 }
 
 /**
  * Translation function for plural translations.
  *
- * @param string $strMsgId			Singular string
- * @param string $strMsgId_plural	Plural string
+ * @param string $strmsgid			Singular string
+ * @param string $strmsgid_plural	Plural string
  * @param integer $intNum			Number used to choose which string gets picked.
  * @param string|null $strContext	Context if needed
  * @return string
  */
-function tp($strMsgId, $strMsgId_plural, $intNum, $strContext = null)
+function tp($strmsgid, $strmsgid_plural, $intNum, $strContext = null)
 {
 	if (class_exists("\\QCubed\\I18n\\TranslationService") && TranslationService::instance()->translator()) {
 		if (!defined (I18N_DOMAIN . '__BOUND')) {
@@ -45,16 +45,16 @@ function tp($strMsgId, $strMsgId_plural, $intNum, $strContext = null)
 
 		}
 		return TranslationService::instance()->translatePlural(
-			$strMsgId,
-			$strMsgId_plural,
+			$strmsgid,
+			$strmsgid_plural,
 			$intNum,
 			I18N_DOMAIN,
 			$strContext);
 	}
 	if ($intNum == 1) {
-		return $strMsgId;
+		return $strmsgid;
 	}
 	else {
-		return $strMsgId_plural;
+		return $strmsgid_plural;
 	}
 }
