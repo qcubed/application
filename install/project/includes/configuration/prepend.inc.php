58aaf6789c8c50d634f9d55a7439459d19b4d303
spekary <spekary@gmail.com>
2017-05-17 19:00:42 -0700

    Fixups

--- install/project/includes/configuration/prepend.inc.php
+++ install/project/includes/configuration/prepend.inc.php
@@ -44,20 +44,18 @@ if (!defined('__PREPEND_INCLUDED__')) {
         ->addPsr4('QCubed\\Project\\', QCUBED_PROJECT_DIR . '/qcubed')
         ->addPsr4('QCubed\\Plugin\\', QCUBED_PROJECT_DIR . '/includes/plugins')
         ->addClassmapFile(QCUBED_APP_INCLUDES_DIR . '/app_includes.inc.php')
-        // temp includes
-        ->addPsr4('QCubed\\', QCUBED_BASE_DIR . '/application/src')
-        ->addPsr4('QCubed\\', QCUBED_BASE_DIR . '/common/src')
-        ->addPsr4('QCubed\\', QCUBED_BASE_DIR . '/orm/src')
-        ->addPsr4('QCubed\\I18n\\', QCUBED_BASE_DIR . '/i18n/src')
-        ->addPsr4('QCubed\\Cache\\', QCUBED_BASE_DIR . '/cache/src')
-        ->addPsr4('QCubed\\Codegen\\Generator\\', QCUBED_BASE_DIR . '/application/codegen/generator')
     ;
 
-    if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')) {
-        \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php');
-    }
-    if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php')) {
-        \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php');
+    if (!defined('QCUBED_CODE_GENERATING')) {
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')) {
+            \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php');
+        }
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php')) {
+            \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php');
+        }
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php')) {
+            require_once(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php');
+        }
     }
 
     // Register the custom autoloader, making sure we go after the previous autoloader
@@ -81,7 +79,6 @@ if (!defined('__PREPEND_INCLUDED__')) {
 
     require_once(QCUBED_BASE_DIR . '/i18n/tools/i18n-app.inc.php'); // Include the translation shortcuts. See the Application for translation setup.
     require_once(QCUBED_BASE_DIR . '/application/src/utilities.inc.php');     // Shortcuts used throughout the framework
-    require_once(QCUBED_BASE_DIR . '/orm/src/model_includes.inc.php');     // Shortcuts used throughout the framework
 
     ////////////////////////////////////////////////
     // Initialize the Application and DB Connections
--- test/travis-config.inc.php
+++ test/travis-config.inc.php
@@ -1,17 +1,37 @@
 <?php
 
 /**
- * The base configuration file for the travis test. This is set as the bootstrap file in the phpunit.xml file.
+ * The base configuration file for the travis test.
  */
 $workingDir = getcwd();
 define('__WORKING_DIR__', $workingDir);
+define('APP_DIR', __WORKING_DIR__);
 
 // Configure
-require( __WORKING_DIR__ . '/test/travis/prepend.inc.php');
+require( __WORKING_DIR__ . '/project/includes/configuration/prepend.inc.php');
 
-define ('QCUBED_CONFIG_DIR', __WORKING_DIR__ . '/test/travis');
+\QCubed\AutoloaderService::instance()
+    ->addPsr4('QCubed\\', __WORKING_DIR__ . '/src')    // Add in our own code
+    ->addPsr4("QCubed\\Codegen\\Generator\\", __WORKING_DIR__ . '/codegen/generator/')
+;
 
-require(QCUBED_CONFIG_DIR . '/CodegenBase.php');
+//codegen
+$db = getenv("DB");
+if (!$db) {
+    $db = 'mysql';  // default to mysql, just for local testing of travis build
+}
+require_once (__DIR__ . '/travis/' . $db . '.inc.php');
+require_once(QCUBED_PROJECT_DIR . '/qcubed/Codegen/CodegenBase.php');
 require( QCUBED_ORM_TOOLS_DIR . '/codegen.cli.php');
 
-require_once(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php');
+// Add in the built files that were missed from prepend.inc.php because they didn't exist before codegen
+
+if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')) {
+    \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php');
+}
+if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php')) {
+    \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php');
+}
+if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php')) {
+    require_once(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php');
+}
