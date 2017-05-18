aaa32936b147a13eb97ff9f226919b4d9a937a89
spekary <spekary@gmail.com>
2017-05-17 22:11:03 -0700

    travis

--- test/travis/project/qcubed/Codegen/CodegenBase.php
+++ test/travis/project/qcubed/Codegen/CodegenBase.php
@@ -30,8 +30,8 @@ class CodegenBase extends \QCubed\Codegen\CodegenBase
         // Specify the paths to your template files here. These paths will be searched in the order declared, to
         // find a particular template file. Template files found lower down in the order will override the previous ones.
         static::$TemplatePaths = array(
-            $dirBase . '/application/vendor/qcubed/orm/codegen/templates/',
-            $dirBase . '/application/codegen/templates/',
+            $dirBase . '/vendor/qcubed/application/codegen/templates/',
+            $dirBase . '/vendor/qcubed/orm/codegen/templates/'
         );
     }
 
