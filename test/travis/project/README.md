ec07c2fbb322abeb8819879a7bd299850d08f079
spekary <spekary@gmail.com>
2017-05-17 19:00:27 -0700

    Moving to an install directory

---
+++ test/travis/project/README.md
@@ -0,0 +1,3 @@
+# Travis replacements
+This directory has replacement files for the project directory just for the travis 
+build. This will overwrite anything found there.
--- test/travis/project/includes/configuration/prepend.inc.php
+++ test/travis/project/includes/configuration/prepend.inc.php
@@ -15,12 +15,8 @@ if (!defined('__PREPEND_INCLUDED__')) {
      */
     require(__DIR__ . '/configuration.inc.php');
 
-    // this is a somewhat unique setup. The application code we are trying to test is outside this directory, but everything else is in the vendor directory
-
-    define ('__QC__', __DIR__ . '/../../../');
-
-    require_once(__QC__ . '/application/src/version.inc.php');     // Include the hard-coded QCubed version number
-    require_once(__QC__ . '/application/src/Error/Manager.php');   // Include the error manager so we can process errors immediately
+    require_once(APP_DIR . '/src/version.inc.php');     // Include the hard-coded QCubed version number
+    require_once(APP_DIR . '/src/Error/Manager.php');   // Include the error manager so we can process errors immediately
     \QCubed\Error\Manager::initialize();
 
     //////////////////////////////
@@ -30,22 +26,21 @@ if (!defined('__PREPEND_INCLUDED__')) {
     \QCubed\AutoloaderService::instance()
         ->initialize(dirname(QCUBED_BASE_DIR) )   // register with the vendor directory
         ->addPsr4('QCubed\\Project\\', QCUBED_PROJECT_DIR . '/qcubed')
-        ->addPsr4('QCubed\\Plugin\\', QCUBED_PROJECT_DIR . '/includes/plugins')
         ->addClassmapFile(QCUBED_APP_INCLUDES_DIR . '/app_includes.inc.php')
         // temp includes
-        ->addPsr4('QCubed\\', __QC__ . '/application/src')
-        ->addPsr4('QCubed\\', QCUBED_BASE_DIR . '/common/src')
-        ->addPsr4('QCubed\\', QCUBED_BASE_DIR . '/orm/src')
-        ->addPsr4('QCubed\\I18n\\', QCUBED_BASE_DIR . '/i18n/src')
-        ->addPsr4('QCubed\\Cache\\', QCUBED_BASE_DIR . '/cache/src')
-        ->addPsr4('QCubed\\Codegen\\Generator\\', __QC__ . '/application/codegen/generator')
+        ->addPsr4('QCubed\\', APP_DIR . '/src')
     ;
 
-    if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')) {
-        \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php');
-    }
-    if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php')) {
-        \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php');
+    if (!defined('QCUBED_CODE_GENERATING')) {
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')) {
+            \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php');
+        }
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php')) {
+            \QCubed\AutoloaderService::instance()->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_type_class_paths.inc.php');
+        }
+        if (file_exists(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php')) {
+            require_once(QCUBED_PROJECT_MODEL_GEN_DIR . '/QQN.php');
+        }
     }
 
     // Register the custom autoloader, making sure we go after the previous autoloader
@@ -68,8 +63,7 @@ if (!defined('__PREPEND_INCLUDED__')) {
     // Include any other include files (if any) here...
 
     require_once(QCUBED_BASE_DIR . '/i18n/tools/i18n-app.inc.php'); // Include the translation shortcuts. See the Application for translation setup.
-    require_once(__QC__ . '/application/src/utilities.inc.php');     // Shortcuts used throughout the framework
-    //require_once(QCUBED_BASE_DIR . '/orm/src/model_includes.inc.php');     // Shortcuts used throughout the framework
+    require_once(APP_DIR . '/src/utilities.inc.php');     // Shortcuts used throughout the framework
 
     ////////////////////////////////////////////////
     // Initialize the Application and DB Connections
@@ -77,5 +71,5 @@ if (!defined('__PREPEND_INCLUDED__')) {
     \QCubed\Database\Service::initializeDatabaseConnections();
     \QCubed\Project\Application::instance()->initializeServices();
 
- //   \QCubed\Project\Application::startOutputBuffering();
+    \QCubed\Project\Application::startOutputBuffering();
 }
