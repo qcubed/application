7b0d23dca0ec9f9558853ff66b50029c2a555c7e
spekary <spekary@gmail.com>
2017-06-03 11:41:36 -0700

    Fixing configuration file load order

--- install/project/includes/configuration/configuration.inc.php
+++ install/project/includes/configuration/configuration.inc.php
@@ -8,22 +8,16 @@
  */
 
 if (!defined('QCUBED_CONFIG')) {
-	define ("QCUBED_CONFIG", __FILE__);	// notify other scripts that the config file is loaded, and prevent multiple loads
+    define ("QCUBED_CONFIG", __FILE__);	// notify other scripts that the config file is loaded, and prevent multiple loads
     define ('QCUBED_CONFIG_DIR', __DIR__);
 
-	$dirpath = dirname(__FILE__);
-	$dirpath = realpath($dirpath . '/active');
-	if ($dirpath !== false) {	// does the active directory exist?
-		if (file_exists($dirpath . '/config.cfg.php')) {
-			require ($dirpath . '/config.cfg.php');
-		}
-		foreach (new DirectoryIterator($dirpath) as $fileInfo) {
-			if($fileInfo->isDot()) continue;
-			if ($fileInfo->getFilename() === 'config.cfg.php') continue;
-			$strFileName = $fileInfo->getPathname();
-			if (substr($strFileName, -8) == '.cfg.php') {
-				require ($strFileName);
-			}
-		}
-	}
+    $dirpath = dirname(__FILE__);
+    $dirpath = realpath($dirpath . '/active');
+    if ($dirpath !== false) {	// does the active directory exist?
+        foreach (scandir($dirpath) as $strFileName) {
+            if (substr($strFileName, -8) == '.cfg.php') { // files are read in ascii alphabetical order
+                require ($dirpath . '/' . $strFileName);
+            }
+        }
+    }
 }
