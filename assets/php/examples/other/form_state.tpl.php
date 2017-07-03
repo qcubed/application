48790bdbda0f774b12a9bbc86ac6533019ee883d
spekary <spekary@gmail.com>
2017-07-03 13:26:37 -0700

    formstate doc updates

--- assets/php/examples/other/form_state.tpl.php
+++ assets/php/examples/other/form_state.tpl.php
@@ -17,14 +17,25 @@
 	<p>Since the FormState handler is encapsulated in its own class, you can even define your own formstate
 		handler.</p>
 
-	<p>In our example below, we use <strong>SessionHandler</strong> to store the formstate data in PHP Session, and we
-		will only store the session key (in this case, just a simple integer) on the page as a hidden form variable.
-		For an added level of security, we will also encrypt the key.</p>
+	<p>In our example here, we use <strong>SessionHandler</strong> to store the formstate data in the PHP Session, and we
+		will only store the session key (in this case, a short string) on the page as a hidden form variable.</p>
 
 	<p>If you use your browser's "View Source" functionality, you will see that the <strong>Qform__FormState</strong> hidden
 		form variable is now a <strong>lot</strong> shorter (likely about 20 bytes).  Compare this to the
 		<a href="../basic_qform/intro.php" class="bodyLink">first example</a> where the form state was easily over 1 KB.  This is because
 		the bulk of the form state is being stored as a PHP Session Variable, which is located on the server, itself.</p>
+
+    <p>The following formstate handlers are available to you:</p>
+    <ul>
+        <li>DefaultHandler: Stores the formstate as hidden input on the form</li>
+        <li>DatabaseHandler: Places the formstate in a SQL database</li>
+        <li>FileHandler: Stores formstates as files in a folder, similar to how PHP stores sessions by default</li>
+        <li>SessionHandler: Stores the formstate as variable in the PHP Session</li>
+        <li>RedisHandler: Stores the formstate in a Redis database</li>
+    </ul>
+
+    <p>See the Formstate.cfg.php file in the /project/includes/configuration/active directory for details on how to choose
+    which formstate handler to use and configure it.</p>
 </div>
 
 <div id="demoZone">
