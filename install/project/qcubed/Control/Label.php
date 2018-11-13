637b6a24606487667ae146c42bbd6d58056689e1
Matthias Van Woensel <matthias@insypro.com>
2018-11-13 20:48:32 +0100

    #19 Adding emtpy Label and WaitIcon to project folder to make it consistent to other classes.

---
+++ install/project/qcubed/Control/Label.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace QCubed\Project\Control;
+
+/**
+ * Class Label
+ *
+ * @package QCubed\Project\Control
+ * @was QLabel
+ */
+class Label extends \QCubed\Control\Label
+{
+    
+}
---
+++ install/project/qcubed/Control/WaitIcon.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace QCubed\Project\Control;
+
+/**
+ * Class WaitIcon
+ *
+ * @package QCubed\Project\Control
+ * @was QWaitIcon
+ */
+class WaitIcon extends \QCubed\Control\WaitIcon
+{
+
+}
